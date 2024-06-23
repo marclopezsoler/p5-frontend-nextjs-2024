0701b24d3ed670131e823fc947db1c554f896285
Marc <marcls.arbucies@gmail.com>
2024-06-23 14:28:29 +0200

    loading stylized

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <>
+    <main className="w-screen py-16 flex items-center justify-center">
       <p>Loading...</p>
-    </>
+    </main>
   );
 }
