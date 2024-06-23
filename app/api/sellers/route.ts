c477c37a5b1315e161a3de2ff13d735b5a2fd9c1
Marc <marcls.arbucies@gmail.com>
2024-06-24 01:10:43 +0200

    fetched sellers

--- app/api/sellers/route.ts
+++ app/api/sellers/route.ts
@@ -5,16 +5,12 @@ const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
   try {
-    const products = await prisma.product.findMany({
-      include: {
-        seller: true,
-      },
-    });
-    return NextResponse.json(products);
+    const sellers = await prisma.seller.findMany();
+    return NextResponse.json(sellers);
   } catch (error) {
-    console.error("Error fetching products:", error);
+    console.error("Error fetching sellers:", error);
     return NextResponse.json(
-      { error: "Error fetching products" },
+      { error: "Error fetching sellers" },
       { status: 500 }
     );
   }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -11,30 +11,18 @@ type Product = {
   description: string;
   color: string;
   sellerId: number | null;
-  Comment: Comment[];
   seller: Seller | null;
-  Order: Order[];
-};
-
-type Comment = {
-  id: number;
-  text: string;
-  username: string;
-  productId: number;
 };
 
 type Seller = {
   id: number;
   name: string;
-};
-
-type Order = {
-  id: number;
-  productId: number;
+  email: string,
 };
 
 export default function ProductsPage() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [sellers, setSellers] = useState<Seller[]>([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -47,36 +35,27 @@ export default function ProductsPage() {
       }
     };
 
+    const fetchSellers = async () => {
+      try {
+        const response = await fetch("/api/sellers");
+        const data = await response.json();
+        setSellers(data);
+      } catch (error) {
+        console.error("Error fetching sellers:", error);
+      }
+    };
+
     fetchProducts();
+    fetchSellers();
   }, []);
 
   return (
     <main className="m-4">
       <div className="products-container">
+        {sellers.map((seller) => (
+          <a>{seller.name}</a>
+        ))}
         {products.map((product) => (
-          // <Card>
-          //   <li key={product.id}>
-          //     <h2>{product.name}</h2>
-          //     <p>{product.description}</p>
-          //     <p>Price: ${product.price}</p>
-          //     <p>Color: {product.color}</p>
-          //     {product.Comment.length > 0 && (
-          //       <div>
-          //         <h3>Comments:</h3>
-          //         <ul>
-          //           {product.Comment.map((comment) => (
-          //             <li key={comment.id}>
-          //               <p>
-          //                 <strong>{comment.username}:</strong> {comment.text}
-          //               </p>
-          //             </li>
-          //           ))}
-          //         </ul>
-          //       </div>
-          //     )}
-          //     {product.Comment.length === 0 && <p>No comments yet</p>}
-          //   </li>
-          // </Card>
           <ProductCard product={product} />
         ))}
       </div>
